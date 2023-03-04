bc849e40fb35db1591cf4e564ec82991d278771c
soharab007 <soharabhossain943@gmail.com>
2023-03-04 14:05:11 +0600

    navbar deafult

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <Banner></Banner>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div className="mx-5">
       <h3>home</h3>
     </div>
   );
