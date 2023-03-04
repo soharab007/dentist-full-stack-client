4a10248a9afac605b49f356a4876494a426c0379
soharab007 <soharabhossain943@gmail.com>
2023-03-05 03:23:07 +0600

    Lift up state to share data and Available Appointment Component

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import Chair from "../../../assets/images/chair.png";
 import { DayPicker } from "react-day-picker";
-import { format } from "date-fns";
 
-const AppointmentBanner = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
   return (
     <header className="my-6">
       <div className="hero ">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
             src={Chair}
-            className="max-w-sm rounded-lg shadow-2xl"
+            className="max-w-sm  rounded-lg shadow-2xl"
             alt="dentist-chair"
           />
           <div className="mr-6">
@@ -20,7 +18,6 @@ const AppointmentBanner = () => {
               selected={selectedDate}
               onSelect={setSelectedDate}
             />
-            <p>You Selected On: {format(selectedDate, "PP")}</p>
           </div>
         </div>
       </div>
