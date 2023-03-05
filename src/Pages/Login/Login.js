4568354e1dc94be4f9a40cae3e7e0786c2bf47eb
soharab007 <soharabhossain943@gmail.com>
2023-03-05 21:03:40 +0600

    Explore React hook form validation, error handling

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,39 +1,77 @@
 import React from "react";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const handleLogin = (data) => {
+    console.log(data);
+  };
 
   return (
-    <div className="h-[800px]  flex justify-center items-center">
-      <div>
-        <h2 className="text-primary text-xl">login</h2>
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+    <div className="h-[500px]  flex justify-center items-center">
+      <div className=" w-96 p-7">
+        <h2 className="text-primary text-center text-xl">login</h2>
+        <form onSubmit={handleSubmit(handleLogin)}>
           <div className="form-control w-full max-w-xs">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input
               type="email"
-              {...register("email")}
+              {...register("email", { required: "Email Address is required" })}
               className="input input-bordered w-full max-w-xs"
             />
+            {errors.email && (
+              <p className="text-red-600">{errors.email?.message}</p>
+            )}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text">Password</span>
             </label>
             <input
               type="password"
-              {...register("password")}
+              {...register(
+                "password",
+
+                {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be 6 characters or longer",
+                  },
+                }
+              )}
               className="input input-bordered w-full max-w-xs"
             />
+            {errors.password && (
+              <p className="text-red-600">{errors.password?.message}</p>
+            )}
+            <label className="label">
+              <span className="label-text">Forget Password?</span>
+            </label>
           </div>
-          <p>{data}</p>
-          <input type="submit" />
+
+          <input
+            className="btn btn-accent w-full"
+            type="submit"
+            value="Login"
+          />
         </form>
+        <p>
+          New to Dentist?{" "}
+          <Link className="text-secondary" to="/signup">
+            Create an account
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full">Continue With Google</button>
       </div>
     </div>
   );
