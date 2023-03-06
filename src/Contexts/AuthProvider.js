2aebf376f083061386e3dca8ab1dfd702c6da44b
soharab007 <soharabhossain943@gmail.com>
2023-03-06 19:51:32 +0600

    73-9 Private Route and redirect user to the page they wanted to g

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -15,26 +15,31 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const signIn = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
   const updateUser = (userInfo) => {
+    setLoading(true);
     return updateProfile(auth, userInfo);
   };
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       console.log(currentUser);
       setUser(currentUser);
+      setLoading(false);
     });
     return () => unsubscribe();
   }, []);
-  const authInfo = { createUser, signIn, user, updateUser, logOut };
+  const authInfo = { createUser, signIn, user, loading, updateUser, logOut };
   return (
     <authContext.Provider value={authInfo}>{children}</authContext.Provider>
   );
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -6,6 +6,7 @@ import Home from "../../Pages/Home/Home/Home";
 // import Home from "../../Layout/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 const router = createBrowserRouter([
   {
@@ -32,7 +33,12 @@ const router = createBrowserRouter([
   },
   {
     path: "/dashboard",
-    element: <DashBoard></DashBoard>,
+    element: (
+      <PrivateRoute>
+        {" "}
+        <DashBoard></DashBoard>
+      </PrivateRoute>
+    ),
   },
 ]);
 export default router;
