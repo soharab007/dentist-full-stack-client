72fd771f38638aa6526595c62fbcb833c048d8e6
soharab007 <soharabhossain943@gmail.com>
2023-03-06 13:53:07 +0600

    Use RegEx validation in React Hook form

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -48,14 +48,19 @@ const SignUp = () => {
               <span className="label-text">Password</span>
             </label>
             <input
+              type="password"
               {...register("password", {
                 required: "Password is required",
                 minLength: {
                   value: 6,
-                  message: "Password must be 6 characters or longer",
+                  message: "Password must be 6 characters long",
+                },
+                pattern: {
+                  value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+                  message:
+                    "Password must have uppercase, number and special characters",
                 },
               })}
-              type="password"
               className="input input-bordered w-full max-w-xs"
             />
             {errors.password && (
